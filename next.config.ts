2824323e85f457d6fea56430ffefe2f0c0717d6a
Pranjal Panging <pranjalpanging@keemail.me>
2025-12-18 17:20:28 +0530

    feat: add rewrites configuration for project routing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  trailingSlash: true,
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: "/projects/museum-guide/:path*",
+          destination: "https://the-museum-guide-smart-city.vercel.app/:path*",
+        },
+        {
+          source: "/projects/video-converter/:path*",
+          destination: "https://video-mp3-converter.onrender.com/:path*",
+        },
+      ],
+    };
+  },
 };
 
 export default nextConfig;
